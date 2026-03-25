b424e26fb1b060c8dd00ac22db8df081b5fd0cbd
Partha1107 <Rajashwin1107@gmail.com>
2026-03-25 10:22:11 +0530

    Time: 0 ms (100%), Space: 12.4 MB (82.62%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,10 +1,4 @@
 class Solution(object):
     def maximumWealth(self, accounts):
-        max_wealth = 0
-    
-        for customer in accounts:
-            wealth = sum(customer)   # sum of one row
-            max_wealth = max(max_wealth, wealth)
-    
-        return max_wealth
+        return max(sum(customer) for customer in accounts)
         
