29840a9a9cfbbe58ca9cb2b6905723ef9b67dfb6
Ashwin raj . J . J <Rajashwin1107@gmail.com>
2026-04-30 08:22:36 +0530

    Time: 7 ms (97.73%), Space: 25.9 MB (57.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,15 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # Create a set to store numbers we've already seen
-        seen = set()
-        
-        for num in nums:
-            # If the number is already in the set, we found a duplicate
-            if num in seen:
-                return True
-            # Otherwise, add the number to the set
-            seen.add(num)
-            
-        # If the loop finishes without finding a duplicate
-        return False
+        return len(nums) != len(set(nums))
